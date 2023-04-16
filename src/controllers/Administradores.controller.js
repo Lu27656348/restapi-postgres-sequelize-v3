8f4ffc1099ac42a811ee04ee383d8241cb28779a
Lu27656348 <luiscarlossomoza@gmail.com>
2023-04-15 23:32:47 -0400

    administradores fix

--- src/controllers/Administradores.controller.js
+++ src/controllers/Administradores.controller.js
@@ -17,28 +17,28 @@ export const buscarAdministradores = async (req,res) => {
 };
 
 export const crearAdministradores = async (req,res) => {
-    const { cedula_administrador, nombres,apellidos,contrasena } = req.body;
+    const { cedula, nombres,apellidos,password } = req.body;
     const administrador = await Administradores.create({
-        cedula_administrador: cedula_administrador,
+        cedula_administrador: cedula,
         nombres: nombres,
         apellidos: apellidos,
-        contrasena: contrasena
+        contrasena: password
     },{
         fields: ["cedula_administrador","nombres","apellidos","contrasena"]
     });
     res.json(administrador);
 };
 export const actualizarAdministradores = async (req,res) => {
-    const { id_usuario,cedula_administrador, nombres,apellidos,contrasena } = req.body;
+    const { cedula, nombres,apellidos,password } = req.body;
     const administrador = await Administradores.findOne({
         where: {
             id_usuario: id_usuario
         }
     });
-    administrador.cedula_administrador = cedula_administrador;
+    administrador.cedula_administrador = cedula;
     administrador.nombres = nombres;
     administrador.apellidos = apellidos;
-    administrador.contrasena = contrasena;
+    administrador.contrasena = password;
     const actualizar = await administrador.save();
     res.json( { mensaje: "Administrador actualizado correctamente", Administrador: buscar });
 };
